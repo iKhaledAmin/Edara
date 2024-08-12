9b5501369ef27f7c1b9f1a36b3bd1d8d6656fb35
Khaled Amin <mohamedkhaid394@gmail.com>
2024-08-12 20:35:44 +0300

    first commit

---
+++ src/main/java/com/edara/edara/EdaraApplication.java
@@ -0,0 +1,13 @@
+package com.edara.edara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edara/edara/EdaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edara.edara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
